56edbbad7c3f8fe0b69a412dd09f0120f2a965a5
Frost <frostedwolf789@gmail.com>
2022-11-10 15:20:53 -0500

    add: student and course class

--- main.cpp
+++ main.cpp
@@ -1,9 +1,19 @@
+#include <fstream>
 #include <iostream>
+#include <unordered_map>
+#include <vector>
+
 using namespace std;
 
+class student {
+public:
+};
+
+class course {
+public:
+};
+
 int main() {
-
     cout << "HelloWorld!" << endl;
-
     return 0;
 }
