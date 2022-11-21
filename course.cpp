87e90e5af65132b7b050ebd3686b95fd43bd891b
meeroTheo <meerbalochandhammy12@gmail.com>
2022-11-20 20:25:15 -0500

    Add: finish functions in the course class

--- course.cpp
+++ course.cpp
@@ -10,7 +10,14 @@ float course::calFinal(string id) {
     Calculates the final grade a student given their id and
     returns a float which is the final grade.
     */
-    return 0.0;
+    float finalGrade = 0.0;
+    if (cStudents.find(id) == cStudents.end()) {
+        cout << "Student not found" << endl;
+    }
+    // If the student is in the course
+    finalGrade = (get<0>(cStudents[id]) * 0.2) + (get<1>(cStudents[id]) * 0.2) + (get<2>(cStudents[id]) * 0.2) + (get<3>(cStudents[id]) * 0.4);
+
+    return finalGrade;
 }
 string course::getCourseCode() {
     return courseCode;
@@ -19,9 +26,31 @@ void course::addStudent(string student, tuple<short, short, short, short> grades
     cStudents[student] = grades;
 }
 void course::deleteStudent(string student) {
+    cStudents.erase(student);
 }
 void course::changeGrade(string student, int assessment, short grade) {
+    // change one grade
+    switch (assessment) {
+    case 1:
+        get<0>(cStudents[student]) = grade; // get<0> is the first element of the tuple
+        break;
+    case 2:
+        get<1>(cStudents[student]) = grade; // get<1> is the second element of the tuple
+        break;
+    case 3:
+        get<2>(cStudents[student]) = grade;
+        break;
+    case 4:
+        get<3>(cStudents[student]) = grade;
+        break;
+    default:
+        cout << "Invalid assessment number" << endl;
+        break;
+    }
 }
 // overload
 void course::changeGrade(string student, tuple<short, short, short, short> grades) {
+    // change the whole tuple of grades with the new tuple
+
+    cStudents[student] = grades;
 }
