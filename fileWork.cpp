dab9cabd340a85f9059f6ad9ef85685181f5de0b
cscoding59 <117619211+cscoding59@users.noreply.github.com>
2022-11-21 18:31:09 -0500

    Update fileWork.cpp

--- fileWork.cpp
+++ fileWork.cpp
@@ -10,11 +10,14 @@ void fileWork::readCourseFile(unordered_map<string, course> &courseMap, unordere
     while (getline(readCourse, my_str)) {
         // stream to get string of file
         stringstream s_stream(my_str);
+        
+        //execute until end of line
         while (s_stream.good()) {
             string substr;
             // split commas and remove spaces at front of delimited strings
             getline(s_stream, substr, ',');
             substr.erase(0, substr.find_first_not_of(' '));
+            //add to a list
             result.push_back(substr);
         }
         // make tuple of short cast marks
@@ -43,25 +46,32 @@ void fileWork::readNameFile(unordered_map<string, student> &studentMap, ifstream
     while (getline(readName, my_str)) {
         // stream to get string of file
         stringstream s_stream(my_str);
+        
+        //execute until end of line
         while (s_stream.good()) {
             string substr;
             // split commas and remove spaces at front of delimited strings
             getline(s_stream, substr, ',');
             substr.erase(0, substr.find_first_not_of(' '));
+            //add to a list
             result.push_back(substr);
         }
 
         // if student object not already created, create
         bool found = false;
         if (studentMap.find(result[0]) == studentMap.end()) // student object not found in map
-            studentMap[result[0]] = student(result[1], result[0]);
+            studentMap[result[0]] = student(result[1], result[0]); //put student in their place in student map, according to ID
         result.clear();
     }
 }
 
 void fileWork::writeOutput(unordered_map<string, course> &courseMap, unordered_map<string, student> &studentMap, ofstream &myfile){
+    
+    //loop through each entry in the studentMap
     for (auto& [id,s]: studentMap){
+        //iterate over every course the student takes
         for (string code: s.getCourses()){
+            //write Student ID, name, course code and decimal-formatted final grade to the parameter file.
             myfile << id << ", " << s.getName() << ", " << code << ", " << fixed << setprecision(1) << courseMap[code].calFinal(id) << "\n";
         }
     }
