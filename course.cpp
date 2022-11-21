5496de55213c57cfadf39002b96192e8cf6737a4
cscoding59 <117619211+cscoding59@users.noreply.github.com>
2022-11-21 18:06:27 -0500

    Update course.cpp

--- course.cpp
+++ course.cpp
@@ -5,18 +5,20 @@ course::course(string code, string id, tuple<short, short, short, short> grades)
     cStudents[id] = grades;
 }
 float course::calFinal(string id) {
-    /*
-    calFinal:
-    Calculates the final grade a student given their id and
-    returns a float which is the final grade.
-    */
+    
+    //initialize final grade
     float finalGrade = 0.0;
-    if (cStudents.find(id) == cStudents.end()) {
+    
+    if (cStudents.find(id) == cStudents.end()) 
+    {
+        //if student is not found
         cout << "Student not found" << endl;
     }
-    // If the student is in the course
-    finalGrade = (get<0>(cStudents[id]) * 0.2) + (get<1>(cStudents[id]) * 0.2) + (get<2>(cStudents[id]) * 0.2) + (get<3>(cStudents[id]) * 0.4);
-
+    else 
+    {
+        //If the student is in the course
+        finalGrade = (get<0>(cStudents[id]) * 0.2) + (get<1>(cStudents[id]) * 0.2) + (get<2>(cStudents[id]) * 0.2) + (get<3>(cStudents[id]) * 0.4);
+    }
     return finalGrade;
 }
 string course::getCourseCode() {
@@ -35,7 +37,7 @@ void course::changeGrade(string student, int assessment, short grade) {
         get<0>(cStudents[student]) = grade; // get<0> is the first element of the tuple
         break;
     case 2:
-        get<1>(cStudents[student]) = grade; // get<1> is the second element of the tuple
+        get<1>(cStudents[student]) = grade; // get<1> is the second element of the tuple, and so on.
         break;
     case 3:
         get<2>(cStudents[student]) = grade;
@@ -44,13 +46,12 @@ void course::changeGrade(string student, int assessment, short grade) {
         get<3>(cStudents[student]) = grade;
         break;
     default:
-        cout << "Invalid assessment number" << endl;
+        cout << "Invalid assessment number" << endl; //all other possible cases give an error.
         break;
     }
 }
 // overload
 void course::changeGrade(string student, tuple<short, short, short, short> grades) {
     // change the whole tuple of grades with the new tuple
-
     cStudents[student] = grades;
 }
